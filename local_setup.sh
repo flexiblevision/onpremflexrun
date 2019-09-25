8b1d8735920698a7062106de963b0cc366d59a30
alec <alecgoeckel@gmail.com>
2019-09-25 11:33:21 -0700

    update process server env

--- local_setup.sh
+++ local_setup.sh
@@ -27,7 +27,7 @@ docker run -d --name=capdev -p 0.0.0.0:5000:5000 --restart unless-stopped --priv
     -d agoeckel/prod-onprem-backend:latest 
 
 docker run -p 0.0.0.0:80:3000 --restart unless-stopped \
-    --name captureui -e CAPTURE_SERVER=http://capdev:5000 -d --network imagerie_nw \
+    --name captureui -e CAPTURE_SERVER=http://capdev:5000 -e PROCESS_SERVER=http://capdev -d --network imagerie_nw \
      agoeckel/prod-onprem-frontend:latest
 
 docker run -p 8500:8500 -p 8501:8501 --runtime=nvidia --name localprediction  -d -e AWS_ACCESS_KEY_ID=imagerie -e AWS_SECRET_ACCESS_KEY=imagerie -e AWS_REGION=us-east-1 \
