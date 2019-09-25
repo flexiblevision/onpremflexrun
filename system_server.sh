9e0b30d7108f6ba65efb38ef705e9efe7087e277
alec <alecgoeckel@gmail.com>
2019-09-25 10:56:13 -0700

    update

--- system_server.sh
+++ system_server.sh
@@ -3,6 +3,7 @@ apt install -y python3-pip
 apt-get -y install nodejs
 apt-get -y install npm
 npm install forever -g
+pip install python-jose
 pip install Flask
 pip install Flask-RESTful
 pip install Flask-Cors
