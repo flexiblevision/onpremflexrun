94a79ffedc32e2619e0eebae5e501b367ad2f209
alec <alecgoeckel@gmail.com>
2019-09-27 10:43:18 -0700

    add arch detection for setting up arm/x86 images

---
+++ setup/arm_system_setup.sh
@@ -0,0 +1,12 @@
+
+docker run -d --name=capdev -p 0.0.0.0:5000:5000 --restart unless-stopped --privileged -v /dev:/dev -v /sys:/sys \
+    --network imagerie_nw -e ACCESS_KEY=imagerie -e SECRET_KEY=imagerie \
+    -d fvonprem/arm-backend:latest 
+
+docker run -p 0.0.0.0:80:3000 --restart unless-stopped \
+    --name captureui -e CAPTURE_SERVER=http://capdev:5000 -d --network imagerie_nw \
+     fvonprem/arm-frontend:latest
+
+docker run -p 8500:8500 -p 8501:8501 --runtime=nvidia --name localprediction  -d -e AWS_ACCESS_KEY_ID=imagerie -e AWS_SECRET_ACCESS_KEY=imagerie -e AWS_REGION=us-east-1 \
+    --restart unless-stopped --network imagerie_nw  \
+    -t fvonprem/arm-prediction:latest
--- system_server/upgrade_system.sh
+++ system_server/upgrade_system.sh
@@ -1,25 +1,7 @@
-docker cp capdev:/fvonprem/db.json /
-docker cp capdev:/fvonprem/cameras.json /
+ARCH=$(arch)
+if [ "$ARCH" = "aarch64" ]; then
+    sh ../upgrades/arm_system_upgrade.sh
+elif [ "$ARCH" = "x86_64" ]; then
+    sh ../upgrades/x86_system_upgrade.sh
+fi
 
-# update capdev
-docker stop capdev
-docker rm capdev
-docker run -d --name=capdev -p 0.0.0.0:5000:5000 --restart unless-stopped --privileged -v /dev:/dev -v /sys:/sys \
-    --network imagerie_nw -e ACCESS_KEY=imagerie -e SECRET_KEY=imagerie \
-    -d agoeckel/prod-onprem-backend:latest 
-
-docker cp /db.json capdev:/fvonprem/
-docker cp /cameras.json capdev:/fvonprem/
-# update captureui
-docker stop captureui
-docker rm captureui
-docker run -p 0.0.0.0:80:3000 --restart unless-stopped \
-    --name captureui -e CAPTURE_SERVER=http://capdev:5000 -e PROCESS_SERVER=http://capdev -d --network imagerie_nw \
-     agoeckel/prod-onprem-frontend:latest 
-
-#update localprediction
-#docker stop localprediction
-#docker rm localprediction
-#docker run -p 8500:8500 -p 8501:8501 --runtime=nvidia --name localprediction  -d -e AWS_ACCESS_KEY_ID=imagerie -e AWS_SECRET_ACCESS_KEY=imagerie -e AWS_REGION=us-east-1 \
-    #--restart unless-stopped --network imagerie_nw  \
-    #-t agoeckel/onprem-localprediction:latest 
