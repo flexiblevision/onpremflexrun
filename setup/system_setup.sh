8cc264d500985daf64a973a66737e7d452eabc96
alec <alecgoeckel@gmail.com>
2019-10-01 17:30:03 -0700

    add env to system setup

--- setup/system_setup.sh
+++ setup/system_setup.sh
@@ -8,7 +8,7 @@ docker run -d --name=capdev -p 0.0.0.0:5000:5000 --restart unless-stopped --priv
     -d fvonprem/$4-backend:$CAPDEV_VERSION
 
 docker run -p 0.0.0.0:80:3000 --restart unless-stopped \
-    --name captureui -e CAPTURE_SERVER=http://capdev:5000 -d --network imagerie_nw \
+    --name captureui -e CAPTURE_SERVER=http://capdev:5000 -e PROCESS_SERVER=http://capdev -d --network imagerie_nw \
      fvonprem/$4-frontend:$CAPTUREUI_VERSION
 
 docker run -p 8500:8500 -p 8501:8501 --runtime=nvidia --name localprediction  -d -e AWS_ACCESS_KEY_ID=imagerie -e AWS_SECRET_ACCESS_KEY=imagerie -e AWS_REGION=us-east-1 \
