26dbef0e27136355713dcb586825ca7de06ff760
alec <alecgoeckel@gmail.com>
2019-10-17 10:28:04 -0700

    update crontab, update upgrade scripts

--- system_server/upgrade_system.sh
+++ system_server/upgrade_system.sh
@@ -1,7 +1,7 @@
 ARCH=$(arch)
 if [ "$ARCH" = "aarch64" ]; then
     chmod +x $HOME/flex-run/upgrades/system_container_upgrades.sh
-    sh ./upgrades/system_container_upgrades.sh $1 $2 $3 'arm'
+    sh $HOME/flex-run/upgrades/system_container_upgrades.sh $1 $2 $3 'arm'
 elif [ "$ARCH" = "x86_64" ]; then
     chmod +x $HOME/flex-run/upgrades/system_container_upgrades.sh
     sh $HOME/flex-run/upgrades/system_container_upgrades.sh $1 $2 $3 'x86'
