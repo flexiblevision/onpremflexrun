1a528be9f504e22801d9b9251c08951b65215413
agoeckel <alecgoeckel@gmail.com>
2021-02-01 13:10:19 -0800

    handle ftp folder files and bmp tif files

--- system_server/ftp_worker.py
+++ system_server/ftp_worker.py
@@ -18,7 +18,7 @@ job_queue = Queue('default', connection=redis_con)
 job_queue.empty()
 job_collection.drop()
 processed = {}
-directory = "/home/ftp"
+ftp_directory = "/home/ftp"
 
 def insert_job_ref(job_id, filename):
     tn = time.time_ns() // 1000000 
@@ -30,33 +30,48 @@ def insert_job_ref(job_id, filename):
         'status': 'running'
     })
 
+def process_file(directory, filename):
+    subprocess.call(['mv', directory+'/'+filename, directory+'/'+filename.lower()])
+    if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".tif") or filename.endswith(".bmp"):
+        if filename not in processed:
+            extension = filename[-4:]
+            if not filename.startswith( 'ftp_' ):
+                tn = time.time_ns() // 1000000 
+                rename = 'ftp_'+str(tn)+extension #rename file to prevent parsing errors
+                subprocess.call(['mv', directory+'/'+filename, ftp_directory+'/'+rename])
+                filename = rename
+                
+            print('processing: '+filename)
+            processed[filename] = "processing"
+            j = job_queue.enqueue(process_img, filename, job_timeout=99999999, result_ttl=-1)
+            insert_job_ref(j.id, filename)
+    else:
+        #remove files that are not jpg/png
+        os.system('rm '+directory+'/'+filename)
+ 
 
 while True:
-    print('watching: '+directory)
+    print('watching: '+ftp_directory)
     time.sleep(.5)
-    if os.path.exists(directory):
-        if len(os.listdir(directory)):
-            for filename in os.listdir(directory):
-                subprocess.call(['mv', directory+'/'+filename, directory+'/'+filename.lower()])
-                if filename.endswith(".jpg") or filename.endswith(".png"):
-                    if filename not in processed:
-                        extension = filename[-4:]
-                        if not filename.startswith( 'ftp_' ):
-                            tn = time.time_ns() // 1000000 
-                            rename = 'ftp_'+str(tn)+extension #rename file to prevent parsing errors
-                            subprocess.call(['mv', directory+'/'+filename, directory+'/'+rename])
-                            filename = rename
-                            
-                        print('processing: '+filename)
-                        processed[filename] = "processing"
-                        #os.system('sudo docker cp '+directory+'/'+filename+' capdev:/fvbackend/'+filename)
-                        #add file to redis queue
-                        j = job_queue.enqueue(process_img, filename, job_timeout=99999999, result_ttl=-1)
-                        insert_job_ref(j.id, filename)
+    if os.path.exists(ftp_directory):
+        if len(os.listdir(ftp_directory)):
+            for filename in os.listdir(ftp_directory):
+                file_path = ftp_directory+'/'+filename
+                if os.path.isdir(file_path):
+                    #search inside directory       
+                    if len(os.listdir(file_path)) == 0: 
+                        os.system('rm -rf '+file_path)
+                        continue
+                    for subfilename in os.listdir(file_path):
+                        sub_file_path = ftp_directory+'/'+filename+'/'+subfilename
+
+                        if os.path.isdir(sub_file_path):
+                            os.system('rm -rf '+sub_file_path)
+                        else:
+                            process_file(ftp_directory+'/'+filename, subfilename)
                 else:
-                    #remove files that are not jpg/png
-                    os.system('rm '+directory+'/'+filename)
-                    continue
+                    #process file
+                    process_file(ftp_directory, filename)
         else:
             processed = {}
 
