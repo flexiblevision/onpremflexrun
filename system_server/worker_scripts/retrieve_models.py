706cb32c864190ec74ef7f23af73bfb19665007e
agoeckel <alecgoeckel@gmail.com>
2021-09-10 00:48:23 -0700

    add model type for syncing

--- system_server/worker_scripts/retrieve_models.py
+++ system_server/worker_scripts/retrieve_models.py
@@ -27,6 +27,7 @@ def base_path():
     return xavier_ssd if os.path.exists(xavier_ssd) else '/'
 
 BASE_PATH_TO_MODELS = base_path()+'models/'
+LITE_MODEL_TYPES    = ['high_speed']
 
 def create_config_file(data):
     with open (BASE_PATH_TO_MODELS+'model.config', 'a') as f:
@@ -41,6 +42,7 @@ def create_config_file(data):
         f.write('}')
 
 def retrieve_models(data, token):
+    model_type = 'versions' if 'model_type' not in data or data['model_type'] != 'high_speed' else data['model_type']
     #if model.config exists - remove it
     if os.path.exists(BASE_PATH_TO_MODELS+'model.config'):
         os.system("rm -rf "+BASE_PATH_TO_MODELS+'model.config')
@@ -70,13 +72,14 @@ def retrieve_models(data, token):
             os.system("mkdir " + model_folder)
 
         model_data = {'type': model_name, 'versions': []}
+        for model_type in LITE_MODEL_TYPES: model_data[model_type] =[]
 
         #iterate over the models data and request/extract model to models folder
         for version in versions:
             if model_name in exclude_models and version in exclude_models[model_name]:
                 # model has already been downloaded
                 if os.path.exists(model_folder+'/'+str(version)):
-                    model_data['versions'].append(version)
+                    model_data[model_type].append(version)
                     print('model has already been downloaded')
                 else:
                     print('version not found, skipping...', model_folder+'/'+str(version))
@@ -91,26 +94,33 @@ def retrieve_models(data, token):
                             zf.extractall(model_folder)
                         os.system("mv "+model_folder+"/job.json "+model_folder+"/"+str(version))
                         os.system("mv "+model_folder+"/object-detection.pbtxt "+model_folder+"/"+str(version))
-                        model_data['versions'].append(version)
+                        model_data[model_type].append(version)
                     except zipfile.BadZipfile:
                         print('bad zipfile in '+model_folder)
                         
                     os.system("rm -rf "+model_folder+'/model.zip')
 
-        if model_data['versions']:
+        if model_data[model_type]:
             if model_name in models_versions:
-                models_versions[model_name]['versions'] += model_data['versions']
+                models_versions[model_name][model_type] += model_data[model_type]
             else:
                 models_versions[model_name] = model_data
 
     if models_versions:
-        create_config_file(models_versions.values())
+        if model_type not in LITE_MODEL_TYPES:
+            create_config_file(models_versions.values())
+
         save_models_versions(models_versions.values())
-        print('removing old models')
-        os.system("docker exec localprediction rm -rf /models")
-        print('pushing new models to localprediction')
-        os.system("docker cp "+base_path()+"models localprediction:/")
-        os.system("docker restart localprediction")
+        
+        if model_type in LITE_MODEL_TYPES:
+            os.system("docker exec predictlite rm -rf /data/models")
+            print('pushing models into predictlite server')
+            os.system("docker cp "+base_path()+"models predictlite:/data/")
+        else:
+            os.system("docker exec localprediction rm -rf /models")
+            print('pushing new models to localprediction')
+            os.system("docker cp "+base_path()+"models localprediction:/")
+            os.system("docker restart localprediction")
         return True
     else:
         return False
