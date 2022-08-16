0268f4085f57abe4122e9b7c074f1e59cd8d5980
agoeckel <alecgoeckel@gmail.com>
2022-08-16 15:08:05 -0700

    add log limits to containers

--- scripts/system_cleanup.sh
+++ scripts/system_cleanup.sh
@@ -1,4 +1,4 @@
-sudo find /var/lib/docker/containers/ -type f -name “*.log” -delete
+#sudo find /var/lib/docker/containers/ -type f -name “*.log” -delete
 sudo /usr/bin/docker system prune -f
 
 #clear forever process logs
