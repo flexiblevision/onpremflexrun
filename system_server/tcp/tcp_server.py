39ccac652d92973639fc7f1ab77f8c506b1ba722
agoeckel <alecgoeckel@gmail.com>
2024-03-07 12:12:18 -0800

    add tcp pin input state read

--- system_server/tcp/tcp_server.py
+++ system_server/tcp/tcp_server.py
@@ -17,6 +17,8 @@ util_ref = client["fvonprem"]["utils"]
 config_ref = client['fvonprem']['io_configs']
 pin_state_ref = client["fvonprem"]["pin_state"]
 
+#(<direction input=0/output=1>,<pin_index>,<value on=0/off=1>)
+
 def take_action(actions):
     params = ''
     for key in actions.keys():
@@ -35,6 +37,22 @@ def set_pin_state(pin_num, state):
 
     pin_state_ref.update_one(query, {'$set': {pin_key: state}}, True)
     return str(res)
+
+def read_input_state():
+    state = {
+        "inputs": {
+            1: functions.read_gpi(1),
+            2: functions.read_gpi(2),
+            3: functions.read_gpi(3),
+            4: functions.read_gpi(4),
+            5: functions.read_gpi(5),
+            6: functions.read_gpi(6),
+            7: functions.read_gpi(7),
+            8: functions.read_gpi(8)
+        }
+    }
+
+    return state
 
 import platform 
 if platform.processor() != 'aarch64':
@@ -93,6 +111,26 @@ while True:
                         command          = None
                         actions          = None
                         params           = ''
+                        if incoming_command == "help":
+                            help_map = {
+                                "commands": {
+                                    "Read Input Pins": "GPIread",
+                                    "Set Output Pin State (on/off)": ["{\"1\": True}", "{\"1\": False}"],
+                                    "Run Prediction": {
+                                        "Valid Commands (based on your presets)": list(valid_commands.keys()),
+                                        "Format": "{\"cmd1\": {\"did\": \"12345\"}}"
+                                    }
+                                }
+                            }
+
+                            b_help_string = json.dumps(help_map).encode('utf-8')
+                            connections.send(b_help_string)
+                            continue
+                        elif incoming_command == "GPIread":
+                            io_state_str  = read_input_state()
+                            b_io_state = json.dumps(io_state_str).encode('utf-8')
+                            connections.send(b_io_state)
+                            continue
 
                         incoming_command = json.loads(incoming_command)
                         command          = list(incoming_command.keys())[0]
