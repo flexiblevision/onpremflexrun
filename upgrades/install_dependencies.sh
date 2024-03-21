36bd18fab5e6dbfa948b1f08b9ddbf2b66bf9778
agoeckel <alecgoeckel@gmail.com>
2024-03-20 22:03:12 -0700

    update

--- upgrades/install_dependencies.sh
+++ upgrades/install_dependencies.sh
@@ -6,4 +6,5 @@ usermod -aG dialout visioncell
 sudo rm /etc/xdg/autostart/update-notifier.desktop
 apt-mark hold "nvidia*"
 
+python3 $HOME/flex-run/setup/management.py
 pip3 install -r $HOME/flex-run/requirements.txt
