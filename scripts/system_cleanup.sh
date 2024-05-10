8b09abd9e801a93fafbea2525b7a60148afd0555
agoeckel <alecgoeckel@gmail.com>
2024-05-10 13:19:00 -0700

    add crash file cleanup

--- scripts/system_cleanup.sh
+++ scripts/system_cleanup.sh
@@ -1,6 +1,8 @@
 #sudo find /var/lib/docker/containers/ -type f -name “*.log” -delete
 sudo /usr/bin/docker system prune -f
 
+sudo rm /var/crash/*
+
 #clear forever process logs
 for i in /root/.forever/*log; do rm $i; done
 
