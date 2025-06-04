16d15dcce0fc8aa51fcd72c6a59366f37d339e30
alec <alecgoeckel@gmail.com>
2025-06-04 14:51:09 -0700

    remove files that are older than a month

--- scripts/system_cleanup.sh
+++ scripts/system_cleanup.sh
@@ -1,10 +1,10 @@
 #sudo find /var/lib/docker/containers/ -type f -name “*.log” -delete
 sudo /usr/bin/docker system prune -f
 
-sudo rm /var/crash/*
+sudo find /var/crash/ -type f -mtime +30 -delete
 
 #clear forever process logs
-for i in /root/.forever/*log; do rm $i; done
+find /root/.forever/ -type f -name "*.log" -mtime +30 -delete
 
 #clear any efi images
 sudo python3 /root/flex-run/scripts/clean_efi.py
